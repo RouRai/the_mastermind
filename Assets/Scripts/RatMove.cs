388a9cfd78ac123ea8b25f32d3695fc49c6c1b22
Paccle <jaypascual2008@gmail.com>
2025-03-04 13:34:13 -0600

    rat

---
+++ Assets/Scripts/RatMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMove : MonoBehaviour
+{
+
+
+    public Transform[] waypoints;
+    int cur = 0;
+
+    public float speed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate () {
+    // Waypoint not reached yet? then move closer
+    if (transform.position != waypoints[cur].position) {
+        Vector2 p = Vector2.MoveTowards(transform.position,
+                                        waypoints[cur].position,
+                                        speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+    }
+    // Waypoint reached, select next one
+    else cur = (cur + 1) % waypoints.Length;
+}
+
+}
